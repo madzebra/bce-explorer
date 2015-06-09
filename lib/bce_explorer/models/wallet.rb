dff5a2294ad7b4008bb5a3b91b2b5275ded3811e
madzebra <madzebra@users.noreply.github.com>
2015-06-09 20:53:06 +0500

    bug fixing in wallet model

--- lib/bce_explorer/models/wallet.rb
+++ lib/bce_explorer/models/wallet.rb
@@ -8,12 +8,12 @@ module BceExplorer
     end
 
     def merge(addresses)
-      cluster = @wallets.find_one('_id' => {'$in' => addresses})
-      cluster_id = cluster.nil? ? SecureRandom.hex(8) : cluster['_id']
+      wallet = @wallets.find_one(_id: {'$in' => addresses})
+      cluster_id = wallet.nil? ? SecureRandom.hex(8) : wallet['cluster_id']
 
       addresses.each do |address|
-        wallet = {'_id' => address, 'cluster_id' => cluster_id}
-        @wallets.insert(wallet) if @wallets.find_one(wallet).nil?
+        new_wallet = {'_id' => address, 'cluster_id' => cluster_id}
+        @wallets.insert(new_wallet) if @wallets.find_one(_id: address).nil?
       end
     end
   end
