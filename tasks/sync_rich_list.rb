91920f4ab97530a2418acbad05cc26e4d3ca8ced
madzebra <madzebra@users.noreply.github.com>
2015-09-23 21:43:42 +0500

    bugfix in sync code

--- tasks/sync_rich_list.rb
+++ tasks/sync_rich_list.rb
@@ -2,6 +2,9 @@ root = File.expand_path '..', File.dirname(__FILE__)
 
 require 'rubygems'
 require 'bundler/setup'
-require "#{root}/engine"
+
+Bundler.require
+
+require "#{root}/boot"
 
 BceExplorer::Engine.new(root).sync_rich_list
