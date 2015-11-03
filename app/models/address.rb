56fd50e8f8789b5c437ee2201082df9803d0ff7e
madzebra <madzebra@users.noreply.github.com>
2015-11-04 00:11:31 +0500

    updated Address# model:
    * implemented methods for storing sent, received, minted values
    * added #fetch method to return entity for frontend

--- app/models/address.rb
+++ app/models/address.rb
@@ -11,6 +11,20 @@ module BceExplorer
       super dbh
     end
 
+    def add(address, value)
+      self[address] += value
+      do_update 'received', address, value
+    end
+
+    def sub(address, value)
+      self[address] -= value
+      do_update 'sent', address, value
+    end
+
+    def minted(address, value)
+      do_update 'minted', address, value
+    end
+
     # get address balance
     def [](address)
       result = find address
@@ -23,12 +37,28 @@ module BceExplorer
     # set balance
     def []=(address, balance)
       query = { _id: address }
-      update = { '$set' => { balance: balance } }
-      upsert query, update
+      change = { '$set' => { balance: balance } }
+      upsert query, change
+    end
+
+    def fetch(address)
+      doc = find address
+      doc.nil? ? nil : Entities::Address.create_from(doc)
     end
 
     def exists?(address)
       !find(address).nil?
+    end
+
+    private
+
+    def do_update(param, address, value)
+      doc = find address
+      return if doc.nil?
+      new_mint = (doc[param] || 0.0) + value
+      query = { _id: address }
+      change = { '$set' => { param => new_mint } }
+      update query, change
     end
   end
 end
