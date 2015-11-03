144ce938be88d94408806a11481da7a778d893aa
madzebra <madzebra@users.noreply.github.com>
2015-11-04 00:31:21 +0500

    added intergration Address# entity to richlist

--- app/models/richlist.rb
+++ app/models/richlist.rb
@@ -9,6 +9,7 @@ module BceExplorer
       query = {}
       order = { balance: :desc }
       find_order_limit(query, order, count)
+        .map { |doc| Entities::Address.create_from doc }
     end
   end
 end
