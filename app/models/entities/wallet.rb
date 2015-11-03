884c8a2bfdc7719c209a15edaffbfcc138052088
madzebra <madzebra@users.noreply.github.com>
2015-11-04 01:03:24 +0500

    updated Wallet# entity

--- app/models/entities/wallet.rb
+++ app/models/entities/wallet.rb
@@ -3,10 +3,11 @@ module BceExplorer
   module Entities
     Wallet = Struct.new :id,
                         :name,
+                        :size,
                         :balance,
                         :addresses do
       def self.create_from(params = {})
-        new params['id'], params['name'],
+        new params['id'], params['name'], params['size'],
             params['balance'], params['addresses']
       end
     end
