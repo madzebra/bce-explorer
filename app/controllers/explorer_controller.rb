bfbf2b0d2ae8e7db45313b44b99983eeb4992d41
madzebra <madzebra@users.noreply.github.com>
2015-11-04 19:07:02 +0500

    removed cache routines from ExplorerController#
    use db instead now

--- app/controllers/explorer_controller.rb
+++ app/controllers/explorer_controller.rb
@@ -4,14 +4,12 @@ module BceExplorer
     helpers ExplorerHelper
 
     before do
-      bcount = @cache.cache_for('block_count') { @client.block.count.to_s }
-      @block_count = bcount.to_i
+      @block_count = @db.info.blocks
     end
 
     def initialize(coin = nil)
       fail if coin.nil?
       @db = coin.db
-      @cache = coin.cache
       @client = coin.client
       @coin_info = coin.info
       @reports = Reports.new @db
@@ -19,43 +17,31 @@ module BceExplorer
     end
 
     get '/' do
-      @blocks = (0...15).map do |n|
-        @cache.cache_obj_for "block_#{@block_count - n}" do
-          @client.block(@block_count - n).decode
-        end
-      end
+      @blocks = @db.block.last
       haml :index
     end
 
     get '/address/:address' do
       if @client.address(params['address']).valid?
-        @address_info = @cache.cache_obj_for "address_#{params['address']}" do
-          @reports.address.call params['address']
-        end
+        @address_info = @reports.address.call params['address']
       end
       @address_info ||= {}
       haml :address
     end
 
     get '/block/:blk' do
-      @block_info = @cache.cache_obj_for "block_with_tx_#{params['blk']}" do
-        @client.block(params['blk']).decode_with_tx
-      end
+      @block_info = @db.block[params['blk']]
       haml :block
     end
 
     get '/tx/:txid' do
-      @tx_info = @cache.cache_obj_for "tx_#{params['txid']}" do
-        @client.transaction(params['txid']).decode
-      end
+      @tx_info = @db.transaction[params['txid']]
       haml :tx
     end
 
     get '/wallet/:wallet_id' do
       if @db.wallet.exists? params['wallet_id']
-        @wallet_info = @cache.cache_obj_for "wallet_#{params['wallet_id']}" do
-          @reports.wallet.call params['wallet_id']
-        end
+        @wallet_info = @reports.wallet.call params['wallet_id']
       end
       @wallet_info ||= {}
       haml :wallet
@@ -75,25 +61,19 @@ module BceExplorer
 
     get '/wallets' do
       @wallets = @db.wallet.top.map do |wallet|
-        @cache.cache_obj_for "wallet_top_#{wallet['_id']}" do
-          {
-            'id' => wallet['_id'],
-            'name' => @db.wallet.name(wallet['_id']),
-            'size' => @db.wallet.count(wallet['_id']),
-            'balance' => wallet['total']
-          }
-        end
+        {
+          'id' => wallet['_id'],
+          'name' => @db.wallet.name(wallet['_id']),
+          'size' => @db.wallet.count(wallet['_id']),
+          'balance' => wallet['total']
+        }
       end
       haml :wallets
     end
 
     get '/network' do
-      @network_info = @cache.cache_obj_for 'network_info' do
-        @client.network_info
-      end
-      @network_peer = @cache.cache_obj_for 'peer_info' do
-        @client.network_peer_info
-      end
+      @network_info = @client.network_info
+      @network_peer = @client.network_peer_info
       haml :network
     end
 
@@ -113,11 +93,7 @@ module BceExplorer
 
     def top(count)
       @top = count
-      @top_list = @db.address.top(count).map do |address|
-        @cache.cache_obj_for "richlist_#{address['_id']}" do
-          { 'address' => address['_id'], 'balance' => address['balance'] }
-        end
-      end
+      @top_list = @db.richlist.top(count)
       haml :richlist
     end
   end
